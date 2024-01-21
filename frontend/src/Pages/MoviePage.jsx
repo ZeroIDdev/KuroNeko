3ee0b0bef1498b0c6c03c3a242bf918854500c73
ZeroIDdev <115116337+ZeroIDdev@users.noreply.github.com>
2024-01-21 14:05:47 +0800

    Update MoviePage.jsx

--- frontend/src/Pages/MoviePage.jsx
+++ frontend/src/Pages/MoviePage.jsx
@@ -15,7 +15,7 @@ const MoviePage = (data) => {
 
   const fetchData = async () => {
     try {
-      const response = await fetch(`https://kuro-neko-cbjk.vercel.app/movie/?page=${page}`);
+      const response = await fetch(`https://good-blue-sockeye-wrap.cyclic.app/movie/?page=${page}`);
       const json = await response.json();
       console.log(json);
       if (!error && response.ok) {
